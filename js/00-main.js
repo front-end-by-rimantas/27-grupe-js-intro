13e610bee4957168fe4045272daa2149117c004f
Rimantas <belauzas@yahoo.com>
2021-05-03 11:29:40 +0300

    Initial commit

---
+++ js/00-main.js
@@ -0,0 +1,19 @@
+// vienos eilutes komentaras
+
+/*
+keliu
+eiluciu
+komentaras
+*/
+
+/*
+
+MOKYMOSI TEMOS:
+
+1. kintamieji - atsiminti darbine informacija
+2. palyginimo salygos
+3. ciklai - ejimas per saraso narius
+4. funkcijos - pasikartojancios logikos perpanaudojimas
+5. ivykiai - mouse click, keyboard click, scroll, upload end, page load....
+
+*/

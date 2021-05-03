993669386900167e71324ac3c440b2e40088b4e8
Rimantas <belauzas@yahoo.com>
2021-05-03 12:32:01 +0300

    Variables

---
+++ js/01-variables.js
@@ -0,0 +1,55 @@
+/*
+KINTAMIEJI:
+
+tipai, pagal iniciavimo buda:
+- const (default)
+- let
+- var (nenaudoti)
+
+pagal informacijos tipa:
+- number (skaicius)
+- string (tekstas)
+- boolean (logine reiksme)
+- array (sarasas)
+- object (objektas)
+- null
+- undefined
+
+*/
+
+const amzius = 99;
+const vardas = 'Vardenis';
+const buvoDuse = true;
+const dabarYraNaktis = false;
+const dabarSninga = true;
+const pazymiai = [5, 2, 10, 8, 7];
+const oroTemperatura = [5, 2, 10, 8, 7];
+const draugai = ['Maryte', 'Petras', 'Alphas'];
+const abecele = ['a', 'b', 'c', 'd'];
+const maryte = {
+    vardas: 'Maryte',
+    amzius: 87,
+    megstamaSpalva: 'green',
+    baigtaKlasiu: 12,
+    gyvenamojiVieta: {
+        miestas: 'Palanga',
+        gatve: 'Basanaviciaus',
+        namoNumeris: 1
+    }
+}
+
+const pirmasSkaicius = 0;
+console.log(pirmasSkaicius);
+
+const rankos = 2;
+console.log(rankos);
+
+const stipinuKiekis = 40;
+console.log(stipinuKiekis);
+
+const neigiamiSkaiciai = [-5, -8, -1, -0.0001, -999];
+console.log(neigiamiSkaiciai);
+
+const pazymys_1 = 5;
+const pazymys_2 = 2;
+const pazymys_3 = 10;

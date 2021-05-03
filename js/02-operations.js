0a133fab1a40c758337c7dd9411d495efda918a0
Rimantas <belauzas@yahoo.com>
2021-05-03 12:58:12 +0300

    Operations

---
+++ js/02-operations.js
@@ -0,0 +1,41 @@
+/*
+MATEMATINES OPERACIJOS: +, -, /, *, ()
+
+*/
+
+const pirmas = 5;
+const antras = 8;
+const trecias = -7;
+
+const suma = pirmas + antras + trecias;
+
+console.log(pirmas);
+console.log(antras);
+console.log(trecias);
+console.log('suma lygu');
+console.log(suma);
+
+console.log('--------------------');
+
+const vardas = 'Vardenis';
+const pavarde = 'Pavardenis';
+
+const fullName = vardas + ' ' + pavarde;
+
+console.log(vardas);
+console.log(pavarde);
+console.log(fullName);
+
+const introduction = 'Hi, my name is ' + vardas + '!';
+console.log(introduction);
+
+console.log('--------------------');
+
+const marks = [10, 2, 8, 4];
+console.log(marks);
+
+const marksSum = marks[0] + marks[1] + marks[2] + marks[3];
+console.log(marksSum);
+
+const example = marks[0 + 1 + 2];   // marks[3] -> 4
+console.log(example);

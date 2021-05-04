11f3c9af0f66cbaf639415c09842d4faa6c4e326
Rimantas <belauzas@yahoo.com>
2021-05-04 12:41:42 +0300

    Ternary and length

---
+++ js/05-ternary.js
@@ -0,0 +1,31 @@
+/*
+TERNARY - sutrumpintas if'as
+
+jam yra butinos "true" ir "false" dalys
+
+struktura:
+klausimas ? jei tiesa : jei ne tiesa
+
+*/
+
+const amzius = 144;
+const cenzas = 18;
+
+const arIleisti = amzius >= cenzas ? true : false;
+console.log('Ar ileisti:', arIleisti);
+
+const arVyresnis = amzius >= cenzas ? 'vyresnis' : 'ne vyresnis';
+console.log('Ar vyresnis:', arVyresnis);
+
+const amzius2 = 144;
+const cenzas2 = 18;
+
+let arIleisti2 = null;
+
+if (amzius2 >= cenzas2) {
+    arIleisti2 = true;
+} else {
+    arIleisti2 = false;
+}
+
+console.log('Ar ileisti (2):', arIleisti2);
---
+++ js/06-length.js
@@ -0,0 +1,16 @@
+const hi = 'Labas rytas.';
+const hiSize = hi.length;
+
+// Tekto "Labas" ilgis yra 5 simboliai.
+const ans = `Tekto "${hi}" ilgis yra ${hiSize} simboliai.`;
+
+console.log(ans);
+
+
+const marks = [10, 2, 8, 4, 6];
+const marksCount = marks.length;
+
+// Studentas turi 4 pazymius.
+const student = `Studentas turi ${marksCount} pazymius.`;
+
+console.log(student);

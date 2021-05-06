b8ac751c2c0b6aec2d9fd021df8fd46db31b392d
Rimantas <belauzas@yahoo.com>
2021-05-06 10:35:57 +0300

    String reverse alternatyvos

--- js/12-rev.js
+++ js/12-rev.js
@@ -5,8 +5,16 @@ REVERSE STRING
 const tekstas = 'satyr';
 let reverse = '';
 
-for (let i = tekstas.length - 1; i >= 0; i--) {
-    reverse += tekstas[i];
+for (let i = 0; i < tekstas.length; i++) {
+    reverse = tekstas[i] + reverse;
 }
+
+// for (let i = tekstas.length - 1; i >= 0; i--) {
+//     reverse += tekstas[i];
+// }
+
+// for (let i = 0; i < tekstas.length; i++) {
+//     reverse += tekstas[tekstas.length - i - 1];
+// }
 
 console.log(tekstas, '->', reverse);

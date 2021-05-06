3566de13aaf7d618a7f122bf4694207dba408d52
Rimantas <belauzas@yahoo.com>
2021-05-06 11:48:10 +0300

    Empty

---
+++ js/15-empty.js
@@ -0,0 +1,8 @@
+function tusciaFunkcija() {
+    return false;
+}
+
+console.log(tusciaFunkcija());
+console.log(tusciaFunkcija(984515));
+console.log(tusciaFunkcija('ewar'));
+console.log(tusciaFunkcija(true));
